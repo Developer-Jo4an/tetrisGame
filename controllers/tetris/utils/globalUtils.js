4dcacc414e08ff05710754b78a59921dec021d24
Developer-Jo4an <107997257+Developer-Jo4an@users.noreply.github.com>
2025-03-15 20:25:16 +0300

    fix: размер клетки

--- controllers/tetris/utils/globalUtils.js
+++ controllers/tetris/utils/globalUtils.js
@@ -2,7 +2,7 @@ export const globalUtils = {
   getCellSize: ({gameSize, grid, margin}) => {
     const maxRowLength = grid.reduce((acc, {cells}) => Math.max(cells.length, acc), 0);
     const maxValue = Math.max(maxRowLength, grid.length);
-    return (gameSize.width - 2 * margin) / maxRowLength - maxValue;
+    return (Math.min(gameSize.width, gameSize.height * 0.7) - 2 * margin) / maxRowLength - maxValue;
   },
   generateNumberPairs: (count, partials) => {
     const result = [];
